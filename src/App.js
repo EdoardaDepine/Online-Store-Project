6eeb329edfcedf5149f7d9d8af7deef3531effd2
EdoardaDepine <edoarda.depine@gmail.com>
2023-06-30 10:35:42 -0300

    feat: criando rotas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Component } from "react";
+import { Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/cart' component={Cart} />
+          <Route
+            path='/categorie/:id'
+            render={(propsRouter) => <Categorie {...propsRouter} />}
+          />
+          <Route
+            exact
+            path='/product/:productId'
+            render={(propsRouter) => <ProductDetails {...propsRouter} />}
+          />
+          <Route path='/checkout' component={CheckoutProducts} />
+          <Route path='/checkoutFinalized' component={checkoutFinalized} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
