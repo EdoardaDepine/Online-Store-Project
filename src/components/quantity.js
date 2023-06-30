22176b25e7eabe59d376e48e0cf615d7807bf1f2
EdoardaDepine <edoarda.depine@gmail.com>
2023-06-30 11:19:01 -0300

    feat: criando função alterationProductQuantity

--- src/components/quantity.js
+++ src/components/quantity.js
@@ -17,6 +17,45 @@ class Quantity extends Component {
     product: {},
   };
 
+  alterationProductQuantity = (id, isAdd) => {
+    const { intireProduct } = this.props;
+    const cartProducts = readCartProducts();
+
+    const addProduct = {
+      title: intireProduct.title,
+      price: intireProduct.price,
+      thumbnail: intireProduct.thumbnail,
+      id: intireProduct.id,
+      quantity: 1,
+    };
+
+    if (cartProducts === undefined || cartProducts.length === 0) {
+      return addProductInCart(addProduct);
+    } else {
+      const product = cartProducts.find((product) => product.id === id);
+
+      if (product !== undefined) {
+        removeProductById(product);
+        const newProduct = {
+          title: product.title,
+          price: product.price,
+          thumbnail: product.thumbnail,
+          id: product.id,
+          quantity: isAdd ? (product.quantity += 1) : (product.quantity -= 1),
+        };
+
+        this.setState({ inputQuantity: newProduct.quantity });
+        const getProducts = cartProducts.filter((p) => p !== product);
+        saveProductInCart([...getProducts, newProduct]);
+        if (newProduct.quantity <= 1) {
+          this.setState({ buttonDisabled: true });
+        }
+      } else {
+        addProductInCart(addProduct);
+      }
+    }
+  };
+
   onClickButtonAddItem = (id) => {
     this.alterationProductQuantity(id, true);
     this.setState({ buttonDisabled: false });
