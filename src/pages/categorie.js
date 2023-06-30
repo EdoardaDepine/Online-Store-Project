b1a7a6ad18a9bb30011c2ef4256f7238ca4f6b70
EdoardaDepine <edoarda.depine@gmail.com>
2023-06-30 11:39:22 -0300

    feat: criando  Categorie, para exibição das categorias de produtos

---
+++ src/pages/categorie.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Component } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { getCategorieId } from "../services/api";
+import Product from "../components/product.js";
+import Loading from "../components/loading";
+
+class Categorie extends Component {
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <div>
+        <button onClick={this.onCLickButtonCart}>Carrinho de Compras</button>
+        <button onClick={this.onCLickButtonHome}>Home</button>
+        <h1>Produtos:</h1>
+        <div>{this.productsListCategorie()}</div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Categorie);
