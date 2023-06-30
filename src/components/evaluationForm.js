86473c5732c3a49569a02a3a49ddb50b5cdbdad0
EdoardaDepine <edoarda.depine@gmail.com>
2023-06-30 11:06:54 -0300

    feat: criando função saveEvaluationLocalStorage

--- src/components/evaluationForm.js
+++ src/components/evaluationForm.js
@@ -6,6 +6,51 @@ import {
 } from "../services/formLocalStorage";
 
 class EvaluationForm extends Component {
+  state = {
+    evaluations: [],
+    emailInput: "",
+    messageInput: "",
+  };
+
+  componentDidMount = () => {
+    const evaluationsLocalStorage = getEvaluations();
+    this.setState({ evaluations: evaluationsLocalStorage });
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  exibitionEvaluations = () => {
+    const { evaluations } = this.state;
+    if (evaluations.length === 0) return <p>Sem avaliações sobre o produto</p>;
+    return (
+      <div>
+        {evaluations.map((evaluation) => (
+          <div key={evaluation.email}>
+            <p>Avaliação</p>
+            <p>Usuário:{evaluation.email}</p>
+            <p>{evaluation.message}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  sendEvaluationLocalStorage = () => {
+    const { emailInput, messageInput } = this.state;
+    const evaluation = {
+      email: emailInput,
+      message: messageInput,
+    };
+    if (emailInput === "" && messageInput === "")
+      return alert("Preencha todos os campos para enviar sua avaliação!");
+    return addEvaluationsInCart(evaluation);
+  };
+
   render() {
     return (
       <>
