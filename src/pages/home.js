8bb50a9d92d0c204073a2ca9011cb907f89f1f57
EdoardaDepine <edoarda.depine@gmail.com>
2023-06-30 16:19:20 -0300

    style: implementando estilo na pagina de categorias

--- src/pages/home.js
+++ src/pages/home.js
@@ -80,7 +80,10 @@ class Home extends Component {
     ) : (
       <div className='containerPage'>
         <div className='categories'>
-          <button className='btn btn-primary' onClick={this.onCLickButtonCart}>
+          <button
+            className='btn btn-primary btnCartInHome'
+            onClick={this.onCLickButtonCart}
+          >
             <svg
               xmlns='http://www.w3.org/2000/svg'
               width='16'
@@ -91,7 +94,7 @@ class Home extends Component {
             >
               <path d='M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z' />
             </svg>
-            Carrinho de Compras
+            <p>Carrinho de Compras</p>
           </button>
 
           <h2>CATEGORIAS</h2>
