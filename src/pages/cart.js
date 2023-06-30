8c27b8805f953fecd6c0623ae8a7df3890a01649
EdoardaDepine <edoarda.depine@gmail.com>
2023-06-30 11:31:55 -0300

    feat: criando página do carrinho de compras

---
+++ src/pages/cart.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Component } from "react";
+import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
+import Loading from "../components/loading";
+import { getProductsCart } from "../services/cartLocalStorage";
+import Quantity from "../components/quantity";
+
+class Cart extends Component {
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <div>
+        <p>Carrinho de Compras</p>
+        <button onClick={this.onClickButtonGoBackHome}>Home</button>
+        <div>{this.exibitionProducts()}</div>
+        <button className='goBackHome' onClick={this.onClickButtonGoBackHome}>
+          Voltar
+        </button>
+        <button onClick={this.onClickButtonCheckoutProducts}>
+          Finalizar Compra
+        </button>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Cart);
