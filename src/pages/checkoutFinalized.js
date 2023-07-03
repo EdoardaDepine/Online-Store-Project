e32d669be1667530610d8012503320f28aec06a1
EdoardaDepine <edoarda.depine@gmail.com>
2023-07-03 10:57:44 -0300

    style: aplicando estilo na página de compra finalizada

--- src/pages/checkoutFinalized.js
+++ src/pages/checkoutFinalized.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Component } from "react";
 import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
+import "../style/checkoutFinalized.css";
 
 class CheckoutFinalized extends Component {
   onCLickButtonHome = () => {
@@ -9,9 +10,9 @@ class CheckoutFinalized extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Compra finalizada com sucesso!</h1>
-        <button onClick={this.onCLickButtonHome}>
+      <div className='container'>
+        <h2>Compra finalizada com sucesso!</h2>
+        <button onClick={this.onCLickButtonHome} className='btn btn-success'>
           Voltar à página principal
         </button>
       </div>
